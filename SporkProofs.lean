582d3fad2dedeb64542ad43533c6e6ef2992a542
Colin McDonald <colinmcd0731@gmail.com>
2025-10-28 13:09:59 -0400

    Update library

--- SporkProofs.lean
+++ SporkProofs.lean
@@ -1,9 +1,13 @@
 -- This module serves as the root of the `SporkProofs` library.
 -- Import modules here that should be built as part of the library.
-import SporkProofs.IVec
 import SporkProofs.SimpSet
+import SporkProofs.IVec
+import SporkProofs.HeadIs
 import SporkProofs.Syntax
 import SporkProofs.Semantics
+import SporkProofs.WFSyntax
+import SporkProofs.WFSemantics
+import SporkProofs.Notation
 import SporkProofs.ParReduce
 import SporkProofs.Preservation
 import SporkProofs.Progress
